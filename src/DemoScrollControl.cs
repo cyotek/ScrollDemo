87dade059b00553d341db62e264f0bbe876d0121
Richard Moss <richard.moss@cyotek.com>
2020-10-31 10:50:14 +0000

    FIX: Previous commit broke jumping to end

--- src/DemoScrollControl.cs
+++ src/DemoScrollControl.cs
@@ -645,7 +645,7 @@ namespace Cyotek.Demo.Scroll
     /// <param name="value">  The value to apply. </param>
     private void SetScrollValue(int value)
     {
-      value = Math.Min(value, _scrollBar.Maximum - (_scrollBar.LargeChange + 1));
+      value = Math.Min(value, _scrollBar.Maximum - _scrollBar.LargeChange);
 
       if (value < 0)
       {
